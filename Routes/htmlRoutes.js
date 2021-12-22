5ca850e990ae60a87a78eb77e4e4527161f14a60
Alison Funkhouser <alison.m.funkhouser@gmail.com>
2021-12-21 16:56:52 -0800

    fix file path in html routes

--- Routes/htmlRoutes.js
+++ Routes/htmlRoutes.js
@@ -3,10 +3,10 @@ var path = require("path");
 module.exports = function(app) {
   // Called when "Countinue Workout" or "new Workout" is clicked in index.html
   app.get("/exercise", function(req, res) {
-    res.sendFile(path.join(__dirname, "../public/exercise.html"));
+    res.sendFile(path.join(__dirname, "../Public/exercise.html"));
   });
   // Not quite sure what this is used for yet ....
   app.get("/stats", function(req, res) {
-    res.sendFile(path.join(__dirname, "../public/stats.html"));
+    res.sendFile(path.join(__dirname, "../Public/stats.html"));
   });
 };
