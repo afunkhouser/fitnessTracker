c52916edd00b6d2e74fcc420092d435814a84678
Alison Funkhouser <alison.m.funkhouser@gmail.com>
2021-12-21 17:00:12 -0800

    add app.us to server.js

--- server.js
+++ server.js
@@ -16,9 +16,11 @@ app.use(express.static("public"));
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", { useNewUrlParser: true });
 
 const db = require("./Models ");
+app.use(require('./Routes/apiRoutes'));
+app.use(require('./Routes/htmlRoutes'))
 
-require("./Routes/apiRoutes")(app);
-require("./Routes/htmlRoutes")(app);
+// require("./Routes/apiRoutes")(app);
+// require("./Routes/htmlRoutes")(app);
 
 app.listen(PORT, () => {
   console.log(`App running on port ${PORT}!`);
