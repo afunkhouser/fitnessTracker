afab1bcd0649ed6b7130223c59b5c21fa898255c
Alison Funkhouser <alison.m.funkhouser@gmail.com>
2021-12-21 17:03:45 -0800

    fix casing in server.js

--- server.js
+++ server.js
@@ -11,16 +11,16 @@ const app = express();
 app.use(logger("dev"));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(express.static("public"));
+app.use(express.static("Public"));
 
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", { useNewUrlParser: true });
 
 const db = require("./Models ");
-app.use(require('./Routes/apiRoutes'));
-app.use(require('./Routes/htmlRoutes'))
+// app.use(require('./Routes/apiRoutes'));
+// app.use(require('./Routes/htmlRoutes'))
 
-// require("./Routes/apiRoutes")(app);
-// require("./Routes/htmlRoutes")(app);
+require("./Routes/apiRoutes")(app);
+require("./Routes/htmlRoutes")(app);
 
 app.listen(PORT, () => {
   console.log(`App running on port ${PORT}!`);
